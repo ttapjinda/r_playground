e19f6b491e12ca2a1c2c5a0890df2935ad002cc8
Tayida Tapjinda <tayida@thinkbluedata.com>
2015-11-11 22:44:01 +0700

    GetDataFromAPI function: function to get json data from url with three attempt before terminate (prevent connection loss)

---
+++ get_data_from_api.R
@@ -0,0 +1,22 @@
+getDataFromAPI <- function(url) {
+  data_df <- NULL
+  attempt <- 0
+  while ( is.null(data_df) && attempt < 3){
+    attempt <- attempt + 1
+    try(
+      data_df <- fromJSON(url)
+    )
+  }
+  return (data_df)
+}
+RJSONIO.getDataFromAPI <- function(url) {
+  data_df <- NULL
+  attempt <- 0
+  while ( is.null(data_df) && attempt < 3){
+    attempt <- attempt + 1
+    try(
+      data_df <- RJSONIO::fromJSON(url)
+    )
+  }
+  return (data_df)
+}
